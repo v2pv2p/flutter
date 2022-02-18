375bd7d2273053d869090bd79449e23d4f5e66ed
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-02-18 23:13:43 +0900

    제목 줄 추가

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MaterialFlutterApp extends State<MaterialFlutterApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text('Material Design App')),
     );
   }
 }
