610382554e78597b0eed0376e1795fcc4739975b
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-02-18 21:55:49 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(  //상중하
+       body: Row(
+         children: [
+           Icon(Icons.star),
+           Icon(Icons.star),
+           Icon(Icons.star),
+         ],
+       )
+      )
+    );
+  }
+}
