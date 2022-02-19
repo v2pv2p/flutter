4dc893dea530e58e63dc147ca992f2207f6a312f
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-02-20 00:25:53 +0900

    위젯 여러개 만들기, 가로 가운데 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,12 @@ class _MaterialFlutterApp extends State<MaterialFlutterApp> {
           },
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[Icon(Icons.android), Text('android')],
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
+        child: Center(
+          child: Column(
+            children: <Widget>[Icon(Icons.android), Text('android')],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        )
       )
     );
   }
